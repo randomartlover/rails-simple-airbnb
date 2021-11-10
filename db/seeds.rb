5678f4e65cbde1cbee279d8ab64d580a925b5436
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-10 16:13:32 +0000

    flats controller done, 4 flats seeded, faker gem installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding started'
+4.times do
+  Flat.create!(
+    name: Faker::Space.galaxy,
+    address: Faker::Address.full_address,
+    description: Faker::Emotion.adjective,
+    price_per_night: rand(50..5000),
+    number_of_guests: rand(1..13)
+)
+  puts 'Flat created'
+end
+
+puts 'Seeding done'
